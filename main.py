16b6ecd4841110add754f4bc74fdc325ed96224f
GabrielSantosMedeiros <gabriel7.gs61@gmail.com>
2025-04-05 19:13:27 -0300

    configure main.py and main.qml

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
+import sys
 
 
 if __name__ == '__main__':
-    pass
+    
+    app = QGuiApplication([])
+
+    engine = QQmlApplicationEngine()
+    engine.load('ui/main.qml')
+
+    if not engine.rootObjects():
+        sys.exit(-1)
+
+    sys.exit(app.exec())
